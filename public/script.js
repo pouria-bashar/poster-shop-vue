dc18ee33d0926bbe0cd19fde655097db8dd868e5
Pouria Bashardoust <pouria.bashardoust@livn.world>
2017-03-31 16:49:59 +1100

    Added shopping cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,38 @@
-console.log('It works.');
+
+const price = 8.99;
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [],
+    cart: [],
+  },
+  methods: {
+    addItem: function(index) {
+      this.total+=price;
+      const item = this.items[index];
+      let found = false;
+      for(var i=0; i<this.cart.length; i++) {
+        if(this.cart[i].id.id === item.id){
+          found = true;
+          this.cart[i].qty++;
+        }
+      }
+      if(!found) {
+        this.cart.push({
+          id: item,
+          title: item.title,
+          qty: 1,
+          price,
+        });
+      }
+
+
+    },
+    getItems: function(keyword) {
+      fetch(`/search/${keyword}`).then(response => {
+        response.json().then(items => this.items = items);
+      })
+    }
+  }
+}).getItems('Arsenal');
